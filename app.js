9e26b6a3f7a889df3ba24109d7680defac342bea
ericaleong <59629488+ericaleong@users.noreply.github.com>
2020-03-08 15:15:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,54 +2,49 @@ const path = require('path');
 const express = require('express');
 const fs = require('fs');
 const gallery = require('./gallery');
+const ejs = require('ejs');
+const moment = require('moment');
 
 
 // --- Run express --- //
 const app = express();
 
+app.use(express.static('public/images'));
 
-// --- Run pug run --- //
-app.set('view engine', 'pug');
+
+// --- Run ejs --- //
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+// --- Moment module for the date in footer --- //
+app.locals.twentyTwenty = function(){
+  return moment().format('YYYY');
+}
+
+
 // --- GET endpoint handlers --- //
-
-// app.get('/', function(req, res) {
-//   res.render('index', {gallery});
-// })
-
-
-app.get('/', function(req, res) {
-  res.render('index', {gallery});
-})
-
-
-
-
-
-// app.get('/', function(req, res){
-//   res.render('index', gallery);
-// })
-
-// app.get('/', function( req, res) {
-//   res.json(gallery.id);
-// })
+app.get('/', function(req, res){
+  res.render('index');
+});
 
 app.get('/gallery', function(req, res) {
-  res.json(gallery);
+  res.render('gallery', {gallery});
 });
- 
 
-app.get('/gallery/:id', function(req, res){
-  res.json(gallery.filter(function(gallery){
-    return gallery.id == req.params.id;
-  }));
+app.get('/gallery/:id', function(req, res, next){
+  for(photo of gallery){
+    if(photo.id == req.params.id){
+      res.render('id',{title:`${req.params.id}`})
+      return;
+    }
+  }
+  next();
 });
 
 
 // --- Serving static assets --- //
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // --- 404 error page --- //
@@ -60,7 +55,7 @@ app.use(function(req, res, next){
 
 
 // --- Localhost: 3000 --- //
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4005;
 
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
